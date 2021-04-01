3d6be5c80853fd2f5509d1d54fc207a79c03ce4f
Cassandra <cparisi1290@gmail.com>
2021-04-01 18:35:13 -0400

    add destroy all code

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Theme.destroy_all
+Race.destroy_all
 
 zoombie = Theme.create(name: "Zombie")
 mud = Theme.create(name: "Mud")
