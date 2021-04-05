eb0e17e07774420e07ee84fbca99e76690d39bf8
Cassandra <cparisi1290@gmail.com>
2021-04-05 11:42:33 -0400

    add attribute & additional seed data

--- app/serializers/race_serializer.rb
+++ app/serializers/race_serializer.rb
@@ -1,4 +1,4 @@
 class RaceSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name, :description, :city, :state, :distance, :image_url, :theme_id, :theme, :race_date
+  attributes :name, :description, :city, :state, :distance, :image_url, :theme_id, :theme, :race_date, :website
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,12 @@
 Theme.destroy_all
 Race.destroy_all
 
-zoombie = Theme.create(name: "Zombie")
+zombie = Theme.create(name: "Zombie")
 mud = Theme.create(name: "Mud")
 obstacle = Theme.create(name: "Obstacle")
 
-Race.create(name: "Spartan Sprint", description: "A 5K that will change your life. The Sprint is the first stop for many in their Spartan journey. It’s also an event that some returning Spartans focus on for years to come for its challenges of speed and obstacle intensity over a shorter distance.",  city: "Concord", state: "NC", distance: "5k", image_url: "https://s3-eu-central-1.amazonaws.com/v7-eu-uploads-sp/wp-content/uploads/sites/18/2014/12/31111603/IMG_5457-1-1024x341.jpg", theme_id: obstacle.id
-    #, race_date: 2021-04-10 00:00:00.000000000 +0000)
+Race.create(name: "Spartan Sprint", description: "A classic obstacle course racing experience. The Sprint combines relatively easy trail running with 20 obstacles, held on off-road terrain featuring water and mud. Climb walls, crawl under wire, traverse monkey bars, aim a spear throw, and more.",  city: "Concord", state: "NC", distance: "5k", image_url: "https://s3-eu-central-1.amazonaws.com/v7-eu-uploads-sp/wp-content/uploads/sites/18/2014/12/31111603/IMG_5457-1-1024x341.jpg", theme_id: obstacle.id, race_date: 04-10-2021, website: "https://race.spartan.com/en/race/detail/7091/overview")
+
+Race.create(name: "Zombies, Run!", description: "Only a few have survived the zombie epidemic. You are a Runner en-route to one of humanity’s last remaining outposts. They need your help to gather supplies, rescue survivors, and defend their home. 
+
+And you have another mission — one they don’t know about.",  city: "Virtual", state: "Anywhere", distance: "5k", image_url: "https://zombiesrungame.com/assets/img/5klogo.jpg", theme_id: zombie.id, race_date: 04-05-2021, website: "https://zombiesrungame.com/")
